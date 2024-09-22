e316d9b7e15e754196783d70a76617bc61f64ed4
Mikhail Voznyuk <mikhailv.commerce@gmail.com>
2024-09-22 12:11:54 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My First Gatsvy Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
